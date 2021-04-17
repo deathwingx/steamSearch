83e76faaec48a6c73b031597547de58644a5cfae
deathwingx <bernardinij@outlook.com>
2021-04-17 12:28:29 -0500

    adfa

---
+++ steamSearch/steamSearch.cpp
@@ -0,0 +1,14 @@
+#include "steamSearch.h"
+#include <iostream>
+
+using namespace std;
+
+void searchUser(void *ptr)
+{
+	cout << "User" << endl;
+}
+
+void searchGame(void *ptr)
+{
+	cout << "Game" << endl;
+}
---
+++ steamSearch/steamSearch.h
@@ -0,0 +1,9 @@
+#ifndef STEAMSEARCH_H
+#define STEAMSEARCH_H
+
+extern "C"
+{
+	void searchUser(void *ptr);
+	void searchGame(void *ptr);
+}
+#endif //STEAMSEARCH_H
